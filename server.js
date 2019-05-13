d1905df9b753055e7a3806e6aade3a873fd06a88
Daniel <gtdanniel@gmail.com>
2019-05-13 12:15:45 -0600

    Reconfig heroku

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
-
 const express = require('express')
 const PORT = process.env.PORT || 5000
-const path = require('path');
-const app = express();
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
-});
 
 express()
     .use(express.static(__dirname))
